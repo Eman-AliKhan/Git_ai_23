82f5cb454b3c96b2063e530f818f7949c06470af
EmanAliKhan <p230022@pwr.nu.edu.pk>
2025-03-08 21:11:50 -0800

    EmanAliKhan 8 March 2025 10:15 am

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-print("This is versioning practically")
-<<<<<<< HEAD
-print("But aik post tou bnti hai :)")
-=======
-print("Now Git will become my skill set but I won’t do chichorapan on LinkedIn 😊 but add it as a skill")
->>>>>>> feature/feedback
+print("This is Versioning Practically")
